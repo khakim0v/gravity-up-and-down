2381b594472656d0c252c698c026014db900ae3a
Kirill Khakimov <khakikir@fel.cvut.cz>
2021-05-18 17:18:25 +0200

    Backdrop: Adjust to use camera

--- src/main/java/cz/cvut/fel/khakikir/gravityupdown/engine/entity/Backdrop.java
+++ src/main/java/cz/cvut/fel/khakikir/gravityupdown/engine/entity/Backdrop.java
@@ -1,8 +1,10 @@
 package cz.cvut.fel.khakikir.gravityupdown.engine.entity;
 
+import cz.cvut.fel.khakikir.gravityupdown.engine.Engine;
 import cz.cvut.fel.khakikir.gravityupdown.engine.Time;
+import cz.cvut.fel.khakikir.gravityupdown.engine.asset.image.Sprite;
+import cz.cvut.fel.khakikir.gravityupdown.engine.math.Vec2D;
 
-import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
@@ -10,38 +12,35 @@ import java.awt.image.BufferedImage;
  * Used for showing infinitely scrolling backgrounds.
  */
 public class Backdrop extends MapObject {
-    private BufferedImage image;
-
+    private final BufferedImage image;
     private final int width;
     private final int height;
 
     public Backdrop(String path) {
-        try {
-            this.image = ImageIO.read(getClass().getResourceAsStream(path));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+        this.image = Sprite.loadImage(path);
         this.width = image.getWidth();
         this.height = image.getHeight();
     }
 
     @Override
     public void update() {
-        double x = (position.getX() + velocity.getX() * Time.deltaTime) % width;
-        double y = (position.getY() + velocity.getY() * Time.deltaTime) % height;
-        setPosition(x, y);
+        double x = (position.x + velocity.x * Time.deltaTime) % width;
+        double y = (position.y + velocity.y * Time.deltaTime) % height;
+        position.set(x, y);
     }
 
     @Override
     public void draw(Graphics2D g) {
-        int x = (int) position.getX();
-        int y = (int) position.getY();
+        Vec2D mapScroll = Engine.camera.getScroll();
+        final int x = Math.floorMod((int) (position.x - mapScroll.x), width);
+        final int y = Math.floorMod((int) (position.y - mapScroll.y), height);
+
         // 1 2
         // 3 4
+        // TODO: Use GamePanel.WINDOW_WIDTH and WINDOW_HEIGHT
         g.drawImage(image, 0, 0, x, y, width - x, height - y, width, height, null);
-        g.drawImage(image, x, 0, width, y,0 , height - y, width - x, height, null);
-        g.drawImage(image, 0, y, x, height,width - x , 0, width, height - y, null);
-        g.drawImage(image, x, y, width, height,0 , 0, width - x, height - y, null);
+        g.drawImage(image, x, 0, width, y, 0, height - y, width - x, height, null);
+        g.drawImage(image, 0, y, x, height, width - x, 0, width, height - y, null);
+        g.drawImage(image, x, y, width, height, 0, 0, width - x, height - y, null);
     }
 }
