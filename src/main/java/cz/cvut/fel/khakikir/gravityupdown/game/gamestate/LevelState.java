5669694cc6b9b66370da4557f21ebb7b83c8b8d1
Kirill Khakimov <khakikir@fel.cvut.cz>
2021-05-19 21:59:24 +0200

    GameState: Add player and collisions to LevelState

--- src/main/java/cz/cvut/fel/khakikir/gravityupdown/game/gamestate/LevelState.java
+++ src/main/java/cz/cvut/fel/khakikir/gravityupdown/game/gamestate/LevelState.java
@@ -1,21 +1,19 @@
 package cz.cvut.fel.khakikir.gravityupdown.game.gamestate;
 
 import cz.cvut.fel.khakikir.gravityupdown.engine.Engine;
-import cz.cvut.fel.khakikir.gravityupdown.engine.Time;
 import cz.cvut.fel.khakikir.gravityupdown.engine.asset.image.Sprite;
 import cz.cvut.fel.khakikir.gravityupdown.engine.entity.Backdrop;
 import cz.cvut.fel.khakikir.gravityupdown.engine.entity.Dummy;
 import cz.cvut.fel.khakikir.gravityupdown.engine.entity.MapObject;
+import cz.cvut.fel.khakikir.gravityupdown.engine.entity.MapSprite;
 import cz.cvut.fel.khakikir.gravityupdown.engine.gamestate.GameState;
 import cz.cvut.fel.khakikir.gravityupdown.engine.gamestate.GameStateManager;
-import cz.cvut.fel.khakikir.gravityupdown.engine.handler.Keys;
 import cz.cvut.fel.khakikir.gravityupdown.engine.tile.TileLayer;
 import cz.cvut.fel.khakikir.gravityupdown.game.LevelLoader;
 import cz.cvut.fel.khakikir.gravityupdown.game.main.GamePanel;
 import org.tiledreader.TiledObject;
 
 import java.awt.*;
-import java.awt.event.KeyEvent;
 import java.awt.image.BufferedImage;
 import java.util.Map;
 
@@ -26,12 +24,12 @@ public class LevelState extends GameState {
     private TileLayer spikeLayer;
     private TileLayer decorationLayer;
 
+    // player vars
+    private MapSprite player;
+
     // background and foreground
     private Backdrop backdrop;
     private BufferedImage scanlines;
-
-    private MapObject player;
-    private MapObject endZone;
 
     private MapObject dummy;
 
@@ -45,24 +43,84 @@ public class LevelState extends GameState {
 
     @Override
     public void init() {
+        // Load Sprites
+        player = new MapSprite(0, 0);
+        player.loadGraphic("/images/player_single.png");
+        player.width = 16;
+        player.height = 16;
+
+        // Player Handling
+        player.acceleration.set(-20, 500);
+        player.maxVelocity.set(100, 200);
+        player.facing = MapObject.LEFT;
+
+        // Load The Map
         // TODO: Use conversion from relative paths to absolute
         LevelLoader.loadTiledMap("D:\\[Work]\\Studium\\Projects\\B0B36PJV\\GravityUpDown\\src\\main\\resources\\data\\level1_test.tmx", this);
 
-        backdrop = new Backdrop("/images/backdrop.png");
-        backdrop.setVelocity(0, 0);
-
-        scanlines = Sprite.loadImage("/images/scanlines.png");
-
+        // Camera Setup
         dummy = new Dummy();
         dummy.setPosition(GamePanel.WINDOW_WIDTH / 2.0, GamePanel.WINDOW_HEIGHT / 2.0);
         dummy.setVelocity(240, 240);
-        Engine.camera.follow(dummy);
+        Engine.camera.follow(player);
+
+        // Init Background
+        backdrop = new Backdrop("/images/backdrop.png");
+        backdrop.setVelocity(0, 0);
+
+        // Init Foreground (Scanlines Effect (overlayed))
+        scanlines = Sprite.loadImage("/images/scanlines.png");
+
+        // Finally!
+        Engine.camera.focusOn(player.getMidpoint());
     }
 
     @Override
     public void update() {
         super.update();
         backdrop.update();
+        player.update();
+
+        // Collisions
+        if (wallLayer != null) {
+            Engine.collide(wallLayer, player);
+        }
+
+        if (bounceLayer != null) {
+            Engine.collide(bounceLayer, player, this::onBounceHit);
+        }
+
+        if (spikeLayer != null) {
+            Engine.overlap(spikeLayer, player, this::onSpikeHit);
+        }
+
+        // Player Handling
+        player.flipX = (player.facing != MapObject.RIGHT);
+    }
+
+    private void onBounceHit(MapObject object1, MapObject object2) {
+        if (object1 != player && object2 != player)
+            return;
+
+        double maxVel = Math.abs(player.maxVelocity.x);
+
+        if (player.facing == MapObject.RIGHT) {
+            player.velocity.x = -maxVel;
+            player.facing = MapObject.LEFT;
+        } else if (player.facing == MapObject.LEFT) {
+            player.velocity.x = maxVel;
+            player.facing = MapObject.RIGHT;
+        }
+
+        player.acceleration.x = -player.acceleration.x;
+
+        // TODO:
+        // stats.bounces++;
+        // sndBounce.play(true);
+    }
+
+    private void onSpikeHit(MapObject object1, MapObject object2) {
+        var x = 2;
     }
 
     @Override
@@ -72,6 +130,7 @@ public class LevelState extends GameState {
         bounceLayer.draw(g);
         spikeLayer.draw(g);
         decorationLayer.draw(g);
+        player.draw(g);
         drawScanlines(g);
     }
 
@@ -83,17 +142,22 @@ public class LevelState extends GameState {
 
     @Override
     public void handleInput() {
-        if (Keys.isPressed(KeyEvent.VK_LEFT)) {
-            dummy.position.x -= dummy.velocity.x * Time.deltaTime;
-        } else if (Keys.isPressed(KeyEvent.VK_RIGHT)) {
-            dummy.position.x += dummy.velocity.x * Time.deltaTime;
-        }
-
-        if (Keys.isPressed(KeyEvent.VK_UP)) {
-            dummy.position.y -= dummy.velocity.y * Time.deltaTime;
-        } else if (Keys.isPressed(KeyEvent.VK_DOWN)) {
-            dummy.position.y += dummy.velocity.y * Time.deltaTime;
-        }
+//        double velocity = 100;
+//        if (Keys.isPressed(KeyEvent.VK_LEFT)) {
+//            player.velocity.x = -velocity;
+//        } else if (Keys.isPressed(KeyEvent.VK_RIGHT)) {
+//            player.velocity.x = velocity;
+//        } else {
+//            player.velocity.x = 0;
+//        }
+//
+//        if (Keys.isPressed(KeyEvent.VK_UP)) {
+//            player.velocity.y = -velocity;
+//        } else if (Keys.isPressed(KeyEvent.VK_DOWN)) {
+//            player.velocity.y = velocity;
+//        } else {
+//            player.velocity.y = -0;
+//        }
     }
 
     public void handleLevelProperties(Map<String, Object> levelProperties) {
@@ -115,7 +179,7 @@ public class LevelState extends GameState {
 
         switch (object.getName()) {
             case "player" -> {
-                //player.setPosition(x, y);
+                player.setPosition(x, y);
                 return;
             }
             case "end_zone" -> {
