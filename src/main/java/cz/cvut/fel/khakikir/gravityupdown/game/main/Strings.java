0e54b86a8bbc334d41fb92d4e42327c5a3506230
Kirill Khakimov <khakikir@fel.cvut.cz>
2021-05-19 21:45:47 +0200

    Misc: Move Strings.java to util package

--- src/main/java/cz/cvut/fel/khakikir/gravityupdown/game/main/Strings.java
+++
@@ -1,5 +0,0 @@
-package cz.cvut.fel.khakikir.gravityupdown.game.main;
-
-public class Strings {
-    public static final String GAME_TITLE = "GravityUpDown";
-}
---
+++ src/main/java/cz/cvut/fel/khakikir/gravityupdown/game/util/Strings.java
@@ -0,0 +1,8 @@
+package cz.cvut.fel.khakikir.gravityupdown.game.util;
+
+public final class Strings {
+    public static final String GAME_TITLE = "Gravity Up&Down";
+
+    private Strings() {
+    }
+}
