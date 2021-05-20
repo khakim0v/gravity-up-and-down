c9f389aab5ed140c59ba410361d2152b03db6d50
Kirill Khakimov <khakikir@fel.cvut.cz>
2021-05-20 19:43:39 +0200

    Input: Add bouncePressed() and escapePressed() methods

--- src/main/java/cz/cvut/fel/khakikir/gravityupdown/game/input/Input.java
+++ src/main/java/cz/cvut/fel/khakikir/gravityupdown/game/input/Input.java
@@ -5,11 +5,19 @@ import cz.cvut.fel.khakikir.gravityupdown.engine.handler.Keys;
 import java.awt.event.KeyEvent;
 
 public final class Input {
+    private Input() {
+
+    }
+
     public static boolean flipPressed() {
         return Keys.justPressed(KeyEvent.VK_SPACE);
     }
 
-    private Input() {
+    public static boolean bouncePressed() {
+        return Keys.justPressed(KeyEvent.VK_CONTROL);
+    }
 
+    public static boolean escapePressed() {
+        return Keys.justPressed(KeyEvent.VK_ESCAPE);
     }
 }
