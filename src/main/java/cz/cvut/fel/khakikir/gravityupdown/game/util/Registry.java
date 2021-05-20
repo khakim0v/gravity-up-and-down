c1b69cd2c80acfaf55027f11d2920a8497be1561
Kirill Khakimov <khakikir@fel.cvut.cz>
2021-05-20 19:50:10 +0200

    Registry: Add new entries

--- src/main/java/cz/cvut/fel/khakikir/gravityupdown/game/util/Registry.java
+++ src/main/java/cz/cvut/fel/khakikir/gravityupdown/game/util/Registry.java
@@ -18,7 +18,16 @@ public final class Registry {
 
     public enum Sound {
         MUSIC_BIT("/music/bit.ogg"),
-        SFX_DIE("/sfx/die.wav");
+        SFX_DIE("/sfx/die.wav"),
+        SFX_BOUNCE("/sfx/bounce.wav"),
+        SFX_HIT("/sfx/hit.wav"),
+        SFX_JUMP("/sfx/jump.wav"),
+        SFX_POWERUP_FLIP("/sfx/powerup_flip.wav"),
+        SFX_POWERUP_SAVEPOINT("/sfx/powerup_savepoint.wav"),
+        SFX_POWERUP_SMASH("/sfx/powerup_smash.wav"),
+        SFX_SMASH("/sfx/smash.wav"),
+        SFX_UI("/sfx/ui.wav"),
+        SFX_LOGO("/sfx/logo.wav");
 
         private final String path;
 
@@ -32,11 +41,26 @@ public final class Registry {
     }
 
     public enum Image {
+        // Main
         BACKDROP("/images/backdrop.png"),
         CURSOR("/images/cursor.png"),
         PLAYER("/images/player_single.png"),
         SCANLINES("/images/scanlines.png"),
-        TILESET("/images/tileset.png");
+        TILESET("/images/tileset.png"),
+
+        // HUD
+        HUD_BAR("/images/hud_bar.png"),
+        HUD_BAR_RED("/images/hud_bar_red.png"),
+        HUD_BAR_TICKS("/images/hud_bar_ticks.png"),
+        HUD_BAR_TICK_GREEN("/images/hud_bar_tick_green.png"),
+        TIMER("/images/timer.png"),
+
+        // PowerUps
+        POWER_UP_FLIP("/images/powerup_flip.png"),
+        POWER_UP_BOUNCE("/images/powerup_bounce.png"),
+        POWER_UP_SMASH("/images/powerup_smash.png"),
+        POWER_UP_SAVEPOINT("/images/powerup_savepoint.png"),
+        POWER_UP_STOP("/images/powerup_stop.png");
 
         private final String path;
 
