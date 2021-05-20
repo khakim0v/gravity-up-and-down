ab6fb8b26f9b11998eea589e98a43f8088081759
Kirill Khakimov <khakikir@fel.cvut.cz>
2021-05-20 19:39:13 +0200

    Assets: Refactor AudioPlayer into Sound class

--- src/main/java/cz/cvut/fel/khakikir/gravityupdown/engine/asset/audio/Sound.java
+++ src/main/java/cz/cvut/fel/khakikir/gravityupdown/engine/asset/audio/Sound.java
@@ -5,18 +5,22 @@ import cz.cvut.fel.khakikir.gravityupdown.engine.asset.ResourceException;
 
 import javax.sound.sampled.*;
 import java.io.InputStream;
-import java.util.HashMap;
 
-public class AudioPlayer {
-    private static final HashMap<String, Clip> clips = new HashMap<>();
+public class Sound {
+    private Clip clip;
 
-    public static void load(String name, String path) {
-        if (clips.containsKey(name)) return;
+    public static Sound load(String path) {
+        Sound sound = new Sound();
+        sound.loadPath(path);
+        return sound;
+    }
+
+    public void loadPath(String path) {
         try {
-            InputStream is = AudioPlayer.class.getResourceAsStream(path);
+            InputStream is = Sound.class.getResourceAsStream(path);
             if (is == null) {
                 throw new ResourceException(
-                        String.format("AudioPlayer: No audio resource with the name '%s' is found", name));
+                        String.format("Sound: No audio resource with the name '%s' is found", path));
             }
 
             AudioInputStream ais;
@@ -37,37 +41,39 @@ public class AudioPlayer {
                     baseFormat.getSampleRate(),
                     false);
             AudioInputStream dais = AudioSystem.getAudioInputStream(decodeFormat, ais);
-            Clip clip = AudioSystem.getClip();
-            clip.open(dais);
-            clips.put(name, clip);
+            this.clip = AudioSystem.getClip();
+            this.clip.open(dais);
         } catch (Exception e) {
             throw new ResourceException(e);
         }
     }
 
-    public static void play(String name) {
-        Clip clip = getClip(name);
-        stop(clip);
+    public void play() {
+        if (clip != null) {
+            //stop();
 
-        clip.setFramePosition(0);
-        clip.start();
+            clip.setFramePosition(0);
+            clip.start();
+        }
     }
 
-    public static void loop(String name) {
-        Clip clip = getClip(name);
-        stop(clip);
+    public void loop() {
+        if (clip != null) {
+            stop();
 
-        clip.setFramePosition(0);
-        clip.loop(Clip.LOOP_CONTINUOUSLY);
+            clip.setFramePosition(0);
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+        }
     }
 
-    public static void loop(String name, int start, int end) {
-        Clip clip = getClip(name);
-        stop(clip);
+    public void loop(int start, int end) {
+        if (clip != null) {
+            stop();
 
-        clip.setLoopPoints(start, end);
-        clip.setFramePosition(0);
-        clip.loop(Clip.LOOP_CONTINUOUSLY);
+            clip.setLoopPoints(start, end);
+            clip.setFramePosition(0);
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+        }
     }
 
     public static void setMuted(boolean shouldMute) {
@@ -84,23 +90,11 @@ public class AudioPlayer {
         }
     }
 
-    public static void stop(String name) {
-        Clip clip = getClip(name);
-        stop(clip);
-    }
-
-    private static void stop(Clip clip) {
-        if (clip.isRunning()) {
-            clip.stop();
-        }
-    }
-
-    private static Clip getClip(String name) {
-        Clip clip = clips.get(name);
+    private void stop() {
         if (clip != null) {
-            return clip;
-        } else {
-            throw new ResourceException(String.format("AudioPlayer: Can't find clip with the name '%s'", name));
+            if (clip.isRunning()) {
+                clip.stop();
+            }
         }
     }
 }
