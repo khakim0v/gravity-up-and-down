75c8c094c0834ecf6b0e04b4aa7897733b0e6db0
Kirill Khakimov <khakikir@fel.cvut.cz>
2021-05-21 00:19:05 +0200

    UI: Add LabeledValueText

---
+++ src/main/java/cz/cvut/fel/khakikir/gravityupdown/game/ui/LabeledValueText.java
@@ -0,0 +1,27 @@
+package cz.cvut.fel.khakikir.gravityupdown.game.ui;
+
+import cz.cvut.fel.khakikir.gravityupdown.engine.ui.EngineText;
+
+public class LabeledValueText {
+    public EngineText label;
+    public EngineText text;
+    public int value;
+
+    public int offsetX;
+    public int offsetY;
+
+    private LabeledValueText(String label, String value) {
+        this.label = new EngineText(0, 0, 0, 8, label);
+        this.text = new EngineText(0, 0, 0, 8, value);
+    }
+
+    public LabeledValueText(String label, int value) {
+        this(label, String.valueOf(value));
+        this.value = value;
+    }
+
+    public LabeledValueText(String label, double value) {
+        this(label, String.valueOf((int) value));
+        this.value = (int) value;
+    }
+}
